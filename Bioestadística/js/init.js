f69104696e83a78323d51351b0e4781940f209b9
Kleity <perezaguirrejazmin@gmail.com>
2017-05-29 18:58:06 -0500

    Add files via upload

---
+++ Bioestadística/js/init.js
@@ -0,0 +1,37 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal').modal();
+ });
+ $(document).ready(function(){
+     $('.carousel').carousel();
+   });
+
+//pushpin
+$('.pushpin-demo-nav').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+      top: $target.offset().top,
+      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
+  });
+  // Only necessary for window height sized blocks.
+   html, body, .block {
+     height: 100%;
+   }
+
+  $(document).ready(function(){
+    $('.target').pushpin({
+      top: 0,
+      bottom: 1000,
+      offset: 0
+    });
+  });
